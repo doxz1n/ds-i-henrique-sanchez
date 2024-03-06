f52aa53c614893c5563780c329fb4f83f4008d7d
Henrique Sanchez <henriquealcici@gmail.com>
2024-03-06 19:42:32 -0300

    Criado projeto para trabalhar as principais funções na linguagem Java

---
+++ src/com/exercicicos/Ex_Nome.java
@@ -0,0 +1,8 @@
+package com.exercicicos;
+
+public class Ex_Nome {
+    public static void main (String [] args){
+        String nome = "Henrique";
+        System.out.println("Seu nome é " + nome);
+    }
+}
---
+++ src/com/exercicicos/Ex_Soma.java
@@ -0,0 +1,11 @@
+package com.exercicicos;
+
+public class Ex_Soma {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int res = a + b;
+
+        System.out.println("O valor da some é: " + res);
+    }
+}
