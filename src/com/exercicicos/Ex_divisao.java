3231334e95faccd6887920cbcf001d2a8db4270f
Henrique Sanchez <henriquealcici@gmail.com>
2024-03-06 20:34:42 -0300

    Realiza a divisão das variáveis a e b definidas previamente

---
+++ src/com/exercicicos/Ex_divisao.java
@@ -0,0 +1,11 @@
+package com.exercicicos;
+
+public class Ex_divisao {
+    public static void main(String[] args) {
+        double a = 25;
+        double b = 2;
+        double res = a / b;
+
+        System.out.println("A divisão de " + a + " e " + b + " será: " + res);
+    }
+}
