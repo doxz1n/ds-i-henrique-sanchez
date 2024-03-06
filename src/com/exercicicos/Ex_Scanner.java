d7c6e129be6e469fc93eda98b329e67ce3d62263
Henrique Sanchez <henriquealcici@gmail.com>
2024-03-06 20:36:55 -0300

    Realiza a leitura da entrada do usúario por meio do Scanner e após realiza a multiplicação

---
+++ src/com/exercicicos/Ex_Scanner.java
@@ -0,0 +1,18 @@
+package com.exercicicos;
+
+import java.util.Scanner;
+
+public class Ex_Scanner {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in); //Supervariavel
+        double a,b;
+        System.out.println("Digite o valor de A: ");
+        a = x.nextDouble(); // Leitura da entrada do usuario
+
+        System.out.println("Digite o valor de B");
+        b = x.nextDouble();
+
+        double res = a * b;
+        System.out.println("O resultado da multiplicação de A e B será " + res);
+    }
+}
