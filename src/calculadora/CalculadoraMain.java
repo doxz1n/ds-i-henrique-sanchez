a4929d40d2eaebea4af66699e40bb6dbadb51bd4
Henrique Sanchez <henriquealcici@gmail.com>
2024-03-10 02:58:31 -0300

    Adiciona uma calculadora simples em Java
    Funcionalidades:
    Entrada de valores através de caixas de diálogo JOptionPane.
    Exibição do resultado da operação com o tipo de operação realizado.
    Tratamento de erros de divisão por zero.
    
    Código:
    
    O código da calculadora está organizado em duas classes:
    
    Calc: Contém os métodos para realizar as operações matemáticas e exibir o resultado.
    CalculadoraMain: Classe principal que cria um objeto da classe Calc e chama os métodos para realizar as operações.

---
+++ src/calculadora/CalculadoraMain.java
@@ -0,0 +1,14 @@
+package calculadora;
+
+import javax.swing.*;
+
+public class CalculadoraMain {
+    public static void main(String[] args) {
+        Calc calc = new Calc();
+        calc.entrada();
+        calc.escolha();
+        if (calc.b != 0) {
+            JOptionPane.showMessageDialog(null, "O resultado da " + calc.tipoOperacao + " de " + calc.a + " e " + calc.b + " é " + calc.res);
+        }
+    }
+}
