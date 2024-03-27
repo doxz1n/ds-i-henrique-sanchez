c3ee2b0def0db5b927ff55257022c70618d1dba1
Henrique Sanchez <henriquealcici@gmail.com>
2024-03-27 19:27:54 -0300

    Exercicios de laços de repetição

---
+++ src/aula27_03_24/Enquanto.java
@@ -0,0 +1,11 @@
+package aula27_03_24;
+
+public class Enquanto {
+    public static void main(String[] args) {
+        int x = 0;
+        while(x<=5){
+            System.out.println(x + " teste");
+            x++;
+        }
+    }
+}
---
+++ src/aula27_03_24/Para.java
@@ -0,0 +1,9 @@
+package aula27_03_24;
+
+public class Para {
+    public static void main(String[] args) {
+        for(int x =0; x<=5;x++){
+            System.out.println(x + " teste");
+        }
+    }
+}
